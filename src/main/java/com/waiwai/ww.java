6178c96085a4eb9d081176d8689194dfd4e8ef1c
waiwai000 <15340990647@163.com>
2021-04-15 21:05:32 +0800

    ww

---
+++ src/main/java/com/waiwai/ww.java
@@ -0,0 +1,14 @@
+package com.waiwai;
+
+/**
+ * @author 歪歪
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/4/15 21:04
+ */
+public class ww {
+
+    public static void main(String[] args) {
+        System.out.println("ww");
+    }
+}
