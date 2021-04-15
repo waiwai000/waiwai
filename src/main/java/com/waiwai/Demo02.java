723b5392b14631cea206fe6d48dbf0fde8e9507d
waiwai000 <15340990647@163.com>
2021-04-15 16:51:23 +0800

    22

---
+++ src/main/java/com/waiwai/Demo02.java
@@ -0,0 +1,16 @@
+package com.waiwai;
+
+/**
+ * @author 歪歪
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/4/15 16:41
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+
+        System.out.println("22");
+
+    }
+
+}
