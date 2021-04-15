6e9fb29c9bafe2618b6314b88a1b8cfdcdb57276
waiwai000 <15340990647@163.com>
2021-04-15 20:13:52 +0800

    aaa

---
+++ src/main/java/com/waiwai/Tes02.java
@@ -0,0 +1,16 @@
+package com.waiwai;
+
+/**
+ * @author 歪歪
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/4/15 20:12
+ */
+public class Tes02 {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "bbb"
+        );
+    }
+}
