7176389ad99492adfef6566dbf45f45fb7c8f91e
waiwai000 <15340990647@163.com>
2021-04-15 16:56:39 +0800

    333

---
+++ src/main/java/com/waiwai/Demo03.java
@@ -0,0 +1,15 @@
+package com.waiwai;
+
+/**
+ * @author 歪歪
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/4/15 16:55
+ */
+public class Demo03 {
+
+    public static void main(String[] args) {
+
+        System.out.println("333");
+    }
+}
