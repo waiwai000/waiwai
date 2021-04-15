2a5653f582557d8015416a1ddd894fddf9353223
waiwai000 <15340990647@163.com>
2021-04-15 19:31:35 +0800

    111

---
+++ src/main/java/com/waiwai/test01.java
@@ -0,0 +1,17 @@
+package com.waiwai;
+
+/**
+ * @author 歪歪
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/4/15 19:29
+ */
+public class test01 {
+    public static void main(String[] args) {
+
+        System.out.println(
+                "111"
+        );
+    }
+
+}
